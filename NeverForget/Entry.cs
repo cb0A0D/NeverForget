64ea150f2feae1418f3de98f432efa00e705903a
Mary Silverthorn <msilverthorn@gmail.com>
2022-11-13 05:55:45 -0500

    Entry.cs finished. Working program.

--- NeverForget/Entry.cs
+++ NeverForget/Entry.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointments
+{
+    /// <summary>
+    /// Represents Appointment entry
+    /// </summary>
+    class Entry
+    {
+        /// <summary>
+        /// The Date and time
+        /// </summary>
+        public DateTime Occurs { get; set; }
+        /// <summary>
+        /// Entry text
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Initializes instance
+        /// </summary>
+        /// <param name="occurs">Date and time</param>
+        /// <param name="text">Entry text</param>
+        public Entry(DateTime occurs, string text)
+        {
+            Occurs = occurs;
+            Text = text;
+        }
+
+        /// <summary>
+        /// Returns entry string representation
+        /// </summary>
+        /// <returns>Entry string representation</returns>
+        public override string ToString()
+        {
+            return Occurs + " " + Text;
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 Console Appointment Calendar App in C#
 Final Project for Code Kentucky Software Developement One due Nov 18, 2022
 Very simple app that shows today and tomorrows appointments at the top and allows you to enter an appointment, search for one by date and time, delete future appointments and exit.
-Features
+Features:
 Master Loop 
 Error handling
 Collections(List/Database)
 Multiple classes
 Multiple functions (add, search, delete)
+Collects and returns date/time/appointment
+TryParse, If/Else,Foreach
+
+
+Flaws:
+No retention of information when shut down (is run by the NeverForget.csproj)
+You must hit a key after the program gives you a result to make a choice from the main loop
